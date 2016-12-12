48f753d5784dc5a2115e5fcf54b4a35068b60531
Adrián Martínez <a.martinez1889@gmail.com>
2016-12-12 19:50:04 +0100

    Support for number of twits

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -1,5 +1,8 @@
 package soa.web;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +26,18 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        if (q.matches("^\\S.* max:\\d+$")) {
+            Map<String, Object> headers = new HashMap<String, Object>(2);
+            int index = 0;
+            
+            index = q.lastIndexOf(':');
+            headers.put("CamelTwitterKeywords", q.substring(0, index-5));
+            headers.put("CamelTwitterCount", Integer.parseInt(q.substring(index+1)));
+            
+            return producerTemplate.requestBodyAndHeader("direct:search", "", headers);
+        }
+        else {
+            return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        }
     }
 }
